afb79e0e7d5fae840602ddc3029c930acb10177c
MARGAM VINAY <margamvinay77@gmail.com>
2024-08-13 00:24:15 +0530

    build configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "localhost:3000",
+        port: "3000",
+        pathname: "/**",
+      },
+    ],
+    domains: ["http://localhost:3000/"],
+  },
+};
 
 export default nextConfig;
